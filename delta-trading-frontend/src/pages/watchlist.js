0fc876ea1f964d69a8500e871e3c5e3cd4fddb2f
cw-create <colinwang2001@gmail.com>
2022-01-20 11:53:39 -0500

    new Button for updating ticker

--- delta-trading-frontend/src/pages/watchlist.js
+++ delta-trading-frontend/src/pages/watchlist.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Stock from './stock.js'
+import UpdateButton from './updateButton.js'
 import './watchlist.css'
 import { stocksToTrack, metrics, ERRORCONST } from '../data.js';
 
@@ -114,6 +115,83 @@ const WatchList = () => {
         update();
     }
 
+    const updateTicker = (ticker) => {
+        if (stocks.some(e => e.symbol === ticker)) {
+            const update = async () => {
+                const stockData = await axios.get(
+                    `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${ticker}&apikey=${API_KEY}`
+                );
+    
+                for (var j = 0; j < metrics.length; j++) {
+                    if (stockData.data[metrics[j]] === '-' || stockData.data[metrics[j]] === 'None') {
+                        stockData.data[metrics[j]] = ERRORCONST;
+                    }
+                }
+                console.log(
+                    {
+                        'companyName': stockData.data["Name"],
+                        'description': stockData.data["Description"], 
+                        'industry': stockData.data["Industry"], 
+                        "symbol": stockData.data["Symbol"],
+                        "PEG": stockData.data["PEGRatio"],
+                        'bookValue': stockData.data["BookValue"], 
+                        'divPerShare': stockData.data["DividendPerShare"], 
+                        'divYield': stockData.data["DividendYield"], 
+                        'EPS': stockData.data["EPS"], 
+                        'ROA': stockData.data["ReturnOnAssetsTTM"], 
+                        'ROE': stockData.data["ReturnOnEquityTTM"], 
+                        'quarterEarningsGrowth': stockData.data["QuarterlyEarningsGrowthYOY"], 
+                        'quarterRevenueGrowth': stockData.data["QuarterlyRevenueGrowthYOY"], 
+                        'targetPrice': stockData.data["AnalystTargetPrice"], 
+                        'trailingPE': stockData.data["TrailingPE"], 
+                        'forwardPE': stockData.data["ForwardPE"], 
+                        'priceToSales': stockData.data["PriceToSalesRatioTTM"], 
+                        'priceToBook': stockData.data["PriceToBookRatio"], 
+                        'yearlyHigh': stockData.data["52WeekHigh"], 
+                        'yearlyLow': stockData.data["52WeekLow"],
+                        'fiftyDayMovingAvg': stockData.data["50DayMovingAverage"], 
+                        'twoHundredDayMovingAvg': stockData.data["200DayMovingAverage"]
+                    }
+                )
+                axios.post(
+                    'http://localhost:8000/metrics/stock/', 
+                    {
+                        'companyName': stockData.data["Name"],
+                        'description': stockData.data["Description"], 
+                        'industry': stockData.data["Industry"], 
+                        "symbol": stockData.data["Symbol"],
+                        "PEG": stockData.data["PEGRatio"],
+                        'bookValue': stockData.data["BookValue"], 
+                        'divPerShare': stockData.data["DividendPerShare"], 
+                        'divYield': stockData.data["DividendYield"], 
+                        'EPS': stockData.data["EPS"], 
+                        'ROA': stockData.data["ReturnOnAssetsTTM"], 
+                        'ROE': stockData.data["ReturnOnEquityTTM"], 
+                        'quarterEarningsGrowth': stockData.data["QuarterlyEarningsGrowthYOY"], 
+                        'quarterRevenueGrowth': stockData.data["QuarterlyRevenueGrowthYOY"], 
+                        'targetPrice': stockData.data["AnalystTargetPrice"], 
+                        'trailingPE': stockData.data["TrailingPE"], 
+                        'forwardPE': stockData.data["ForwardPE"], 
+                        'priceToSales': stockData.data["PriceToSalesRatioTTM"], 
+                        'priceToBook': stockData.data["PriceToBookRatio"], 
+                        'yearlyHigh': stockData.data["52WeekHigh"], 
+                        'yearlyLow': stockData.data["52WeekLow"],
+                        'fiftyDayMovingAvg': stockData.data["50DayMovingAverage"], 
+                        'twoHundredDayMovingAvg': stockData.data["200DayMovingAverage"]
+                    }
+                )
+                console.log('POST request for updating data');
+                const resultData = await axios.get(
+                    'http://localhost:8000/metrics/stock/'
+                );
+                setStocks(resultData.data);
+            };
+            update();
+        } else {
+            alert(`${ticker} is not tracked in watchlist`)
+        }
+    }
+
     useEffect(() => {
         axios.get('http://localhost:8000/metrics/stock/').then((res) => {
             getScores(res.data);
@@ -121,41 +199,46 @@ const WatchList = () => {
     }, []);
 
     return (
-        <div>
-            <button onClick={updateData}>Update Data</button>
-            <button onClick={() => getScores(stocks)}>Update Scores</button>
-            <div className='stock-list-wrapper'>
-                {
-                    stocks.map((stock, index) => {
-                        return <div className='stock-wrapper' key={index}>
-                            <Stock
-                                cScore={stock["cScore"]}
-                                companyName={stock["companyName"]}
-                                description={stock["description"]} 
-                                industry = {stock["Industry"]} 
-                                symbol = {stock["symbol"]}
-                                PEG = {stock["PEG"]}
-                                bookValue = {stock["bookValue"]}
-                                divPerShare = {stock["divPerShare"]}
-                                divYield = {stock["divYield"]}
-                                EPS = {stock["EPS"]}
-                                ROA = {stock["ROA"]}
-                                ROE = {stock["ROE"]}
-                                quarterEarningsGrowth = {stock["quarterEarningsGrowth"]}
-                                quarterRevenueGrowth = {stock["quarterRevenueGrowth"]}
-                                targetPrice = {stock["targetPrice"]}
-                                trailingPE = {stock["trailingPE"]}
-                                forwardPE = {stock["forwardPE"]}
-                                priceToSales = {stock["priceToSales"]}
-                                priceToBook = {stock["priceToBook"]}
-                                yearlyHigh = {stock["yearlyHigh"]}
-                                yearlyLow = {stock["yearlyLow"]}
-                                fiftyDayMovingAvg = {stock["fiftyDayMovingAvg"]}
-                                twoHundredDayMovingAvg = {stock["twoHundredDayMovingAvg"]}
-                            />
-                        </div>;
-                    })
-                }
+        <div className="watchlist-wrapper">
+            <div>
+                <button onClick={updateData}>Update Data</button>
+                <button onClick={() => getScores(stocks)}>Update Scores</button>
+                <div className='stock-list-wrapper'>
+                    {
+                        stocks.map((stock, index) => {
+                            return <div className='stock-wrapper' key={index}>
+                                <Stock
+                                    cScore={stock["cScore"]}
+                                    companyName={stock["companyName"]}
+                                    description={stock["description"]} 
+                                    industry = {stock["Industry"]} 
+                                    symbol = {stock["symbol"]}
+                                    PEG = {stock["PEG"]}
+                                    bookValue = {stock["bookValue"]}
+                                    divPerShare = {stock["divPerShare"]}
+                                    divYield = {stock["divYield"]}
+                                    EPS = {stock["EPS"]}
+                                    ROA = {stock["ROA"]}
+                                    ROE = {stock["ROE"]}
+                                    quarterEarningsGrowth = {stock["quarterEarningsGrowth"]}
+                                    quarterRevenueGrowth = {stock["quarterRevenueGrowth"]}
+                                    targetPrice = {stock["targetPrice"]}
+                                    trailingPE = {stock["trailingPE"]}
+                                    forwardPE = {stock["forwardPE"]}
+                                    priceToSales = {stock["priceToSales"]}
+                                    priceToBook = {stock["priceToBook"]}
+                                    yearlyHigh = {stock["yearlyHigh"]}
+                                    yearlyLow = {stock["yearlyLow"]}
+                                    fiftyDayMovingAvg = {stock["fiftyDayMovingAvg"]}
+                                    twoHundredDayMovingAvg = {stock["twoHundredDayMovingAvg"]}
+                                />
+                            </div>;
+                        })
+                    }
+                </div>
+            </div>
+            <div className="update-button-wrapper">
+                <UpdateButton updateFunction={updateTicker}/>
             </div>
         </div>
     );
